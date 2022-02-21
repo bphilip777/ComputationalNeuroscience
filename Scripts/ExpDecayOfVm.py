7c04b69800ab889a55c9939abacf995f344f4092
Brian Philip <bphilip777@gmail.com>
2022-02-21 13:12:50 -0700

    Doubling the time constant, doubles the time it takes for decay. Time constant is the time it takes for the signal to decay by 1/e towards equilibrium.

--- Scripts/ExpDecayOfVm.py
+++ Scripts/ExpDecayOfVm.py
@@ -1,21 +1,56 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-t0, tEnd = 0, 0.5
-dt = 1e-4
+t0, tEnd = 0, 100e-3  # ms
+dt = 1e-3
 nTims = int((tEnd-t0)/dt)+1
 t = np.linspace(t0, tEnd, nTims)
 V0 = np.asarray([-50, -70, -80])*10**-3
 V = np.zeros((len(V0), nTims))
+V1 = V.copy()
 tau_m = 10e-3  # 10 ms
+tau_m1 = 20e-3  # Double the time constant
+El = -70e-3
 
-# Initialize V[0]
+# Initialize voltage values
 for i, v in enumerate(V0):
     V[i][0] = v
+    V1[i][0] = v
 
-for i, v in enumerate(V0):
-    for timestep in range(1, nTims):
-        V[i][timestep] = V[i][timestep] + (V[i][timestep-1] - v) * math.exp(- t[timestep]/tau_m)
+# Compute next voltage values
+for i in range(len(V0)):
+    for j in range(1, nTims):
+        V[i][j] = El + (V[i][0] - El) * math.exp(-t[j]/tau_m)
 
-print(V[0])
+for i in range(len(V0)):
+    for j in range(1, nTims):
+        V1[i][j] = El + (V1[i][0] - El) * math.exp(-t[j]/tau_m1)
+
+fig, axs = plt.subplots(1,3)
+axs[0].plot(t, np.exp(-t/tau_m), 'k-')
+axs[0].plot(t, np.exp(-t/tau_m1), 'b--')
+axs[0].set_xlabel("Time, s")
+axs[0].set_ylabel("Voltage, mV")
+axs[0].set_title(f"Blue: {tau_m}, Black: {tau_m1}")
+
+axs[1].plot(t, V[0], 'k-')
+axs[1].plot(t, V[1], 'k--')
+axs[1].plot(t, V[2], 'k-')
+axs[1].set_xlabel("Time, s")
+axs[1].set_ylabel("Vm, mV")
+axs[1].set_ylim([-80e-3, -50e-3])
+axs[1].set_xlim([0, 0.05])
+axs[1].set_title(f"Tau {tau_m} ms")
+
+axs[2].plot(t, V1[0], 'b-')
+axs[2].plot(t, V1[1], 'b--')
+axs[2].plot(t, V1[2], 'b-')
+axs[2].set_xlabel("Time, s")
+axs[2].set_ylabel("Vm, mV")
+#axs[2].set_ylim([-80e-3, -50e-3])
+#axs[2].set_xlim([0, 0.1])
+axs[2].set_title(f"Tau: {tau_m1} ms")
+
+# Show visual
+plt.show()
